fc52d5828c9d3ab6538a03c48f02aaa47e1b6f95
Mmati1996 <57298066+Mmati1996@users.noreply.github.com>
2020-03-28 15:27:31 +0100

    testy_v1 test for value that is in sequence of length 1

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,7 +1,22 @@
 package edu.iis.mto.bsearch;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BinarySearchTest {
 
+
+    @Test
+    void isInSequenceOfLengthOneTest(){
+        int key = 12;
+        int[] sequence = new int[] {12};
+        BinarySearch bs = new BinarySearch();
+        bs.create();
+        SearchResult sr = bs.search(key,sequence);
+        assertTrue(sr.isFound());
+        assertEquals(key,sequence[sr.getPosition()]);
+    }
 }
+
